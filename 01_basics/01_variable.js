8dfa6de9e1ca804e98d70807ac3bc3576dda6009
codewithnikhilchauhan <nc0778853@gmail.com>
2025-06-02 12:11:52 +0000

    conversion of datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 12345
+let accountEmail="nikhil@google"
+var accountPassword="12387"
+accountCity="Delhi"
+let accountState;
+
+accountEmail="nc077.com"
+accountPassword="26436"
+accountCity="mumbai"
+
+
+console.log(accountId);
+/*
+prefer not to use var
+because of isssue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_basics/03_conversion operation.js
@@ -0,0 +1,31 @@
+let score=true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN (not an number)
+//true => 1; false => 0
+
+let isLoogedIn = 1
+
+let booleanIsLoogedIn = Boolean(isLoogedIn)
+//console.log(booleanIsLoogedIn);
+// 1=> true , 0=>false
+//"" => false
+// "Nikhil" =>true
+
+let someNumber = 33
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+ // ****************operation**********************
+ 
+ 
+ 
