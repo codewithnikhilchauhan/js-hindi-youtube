de8af4d829f1d64d3f1d334e103625a257ea124e
codewithnikhilchauhan <nc0778853@gmail.com>
2025-06-05 04:55:26 +0000

    datatype summary

--- 01_basics/03_conversion operation.js
+++ 01_basics/03_conversion operation.js
@@ -27,5 +27,34 @@ let someNumber = 33
 
  // ****************operation**********************
  
+ let value = 3
+ let negvalue = -value
+ console.log(negvalue);
  
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**2);
  
+ let str1="hello"
+ let str2=" nikhil"
+
+ let str3 = str1 + str2
+ console.log(str3);
+
+ console.log("1" + 2);
+ console.log(1 + "2");
+ // ECMA script used in this code we have to learn about this from google
+ 
+//console.log(true);
+//console.log(+true);
+
+let num1, num2, num3
+
+num1= num2= num3= 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive 
+
+// 7 types: string , number, boolean, null=empty not 0,undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.2
+
+constIsLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+constId = Symbol('123')
+constAnotherId = Symbol('123')
+
+console.log(id === anotherId);
+const bigNumber = 63523738367537
+
+// refrence (non primitive)
+
+//array, objects, functions
+// we write arrays in square bracket
+
+const heros = ["hulk", "captain","marvel"];
+let myobj ={
+    name: "nikhil",
+    age: 20,
+}
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof anotherId);
+
