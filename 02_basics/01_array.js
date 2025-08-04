97315c610d77ee9b1d4259730174317762cbe9f9
codewithnikhilchauhan <nc0778853@gmail.com>
2025-08-04 12:12:51 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,34 @@
+// Array
+
+const myArr = [0, 1, 3, 4, 5]
+const myHeros = ["vindeisel", "paul"]
+
+const myArr2 = new Array (1,2,3,4)
+console.log(myArr[1]);
+console.log(myArr2[2]);
+
+//array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop() it helps in removing the last value in 1to 6
+// myArr.unshift(9). we use this to add value in starting of array
+console.log(myArr);
+
+// console.log(myArr.includes(9)); by this we find we have this no on array or not if not then it will give us no
+// console.log(myArr.indexOf(3));  by this we find out the index in array 
+
+// const newArr = myArr.join() we use this we get array but not in the form of just like first one in string without sqaure bracket
+
+console.log(newArr);
+
+// slice   spice
+
+console.log("A" , myArr);
+
+const myn1 = myArr.slice(2,4)
+
+
+
+
+
