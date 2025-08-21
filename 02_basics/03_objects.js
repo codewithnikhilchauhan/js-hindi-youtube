d309859252ff8f1e5ed01faff3e2e46c082b1428
codewithnikhilchauhan <nc0778853@gmail.com>
2025-08-21 11:11:29 +0000

    configure done

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,28 @@
+//  Singleton
+
+// object literals
+
+const mySym = Symbol("key1")
+const JsUser = {
+    name : "nikhil",
+    "full name" : "Nikhil Chauhan",
+    [mySym] : "mykey1",
+    age :18,
+    location : "delhi",
+    email : "nikhil@google.com",
+    isLoogedIn : false,
+    lastloginDays : ["monday", "saturday"]
+}
+console.log(JsUser.email);
+// we use .email or something to console and find out by using . in it and tehre is another method also in this
+console.log(JsUser["email"]);
+// we use this by do not using . in it we use [] these to find out 
+// in full name we do not use this . method bcz there is string and we can't console that with .method so we use []this method
+console.log(JsUser[mySym]);
+
+JsUser.email = "nikhil@google.com"
+Object.freeze(JsUser)
+// we use this freeze method to freeze that email or something else after this value can't be change
+JsUser.email = "nikhil@yahhoo.com"
+Object.freeze(JsUser)
+
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,47 @@
+// const tinderUser = new Object{} this is sinleton object
+
+const tinderUser = {}
+// and this is non-singletoon object
+tinderUser.id = "123abc"
+tinderUser.name = "nikhil"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+const regularUser = {
+ email : "nikhil@gmail.com",
+ fullName : {
+    userfullname : {
+        firstname : "Mani",
+        lastname : "chauhan"
+    }
+ }
+}
+// console.log(regularUser.fullName.userfullname.lastname);
+// if we want to console anything then we use .notation in it and add anything in it
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {1: "a",2: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+const users = [
+  {
+    id:1,
+    email : "nikhil@gmail.com"
+  },
+  {
+    id:1,
+    email : "nikhil@gmail.com"
+  },
+  {
+    id:1,
+    email : "nikhil@gmail.com"
+  }
+]
+users
+
+
