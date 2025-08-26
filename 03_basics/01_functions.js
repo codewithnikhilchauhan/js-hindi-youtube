e296902e9650836623da6abc184393e818a13a87
codewithnikhilchauhan <nc0778853@gmail.com>
2025-08-26 07:56:54 +0000

    configure done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,29 @@
+
+// Function is a type of data where we put 5 lines or 10 lines anylines which u want and u can put in a bunch and take it to anywhere a copy of that data wherever you want
+
+// in this we can use any name to call the function just like saymyname in it and aftyer that () this is scope in the function 
+
+function sayMyName (){
+console.log("N");
+console.log("I");
+console.log("K");
+console.log("H");
+console.log("I");
+console.log("L");
+}
+// () we call this sign paranthesis if we want to execute this function we simply type the function what we put in the functiion and use parenthesis to execute
+
+// sayMyName()
+
+function addTwoNumbers (number1, number2){
+    console.log(number1 + number2);
+    
+}
+// if we want to add some values in the function so we add like this and call just like we calling in the upper function by its name 
+
+// addTwoNumbers()
+
+// if we do not give any value to the numbers so we get nan but if we give some like this we get value in it addTwoNumbers(3, 4)
+// and in another case we add the value like this addTwoNumbers(3, "4") so we get the ouitput like 34 it will not add the value in it
+
+console.log("Result: ", result)
